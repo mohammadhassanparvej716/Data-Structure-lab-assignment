58aab5f9a9d6a53b5720416063016ee0f1afb4be
mohammadhassanparvej716 <110627027+mohammadhassanparvej716@users.noreply.github.com>
2022-09-09 23:11:45 +0600

    Add files via upload

---
+++ Lab-13(Menu driven Stack implementation)/Lab-13.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 10
+
+int stack_arr[MAX];
+int top = -1;
+
+void push(int item);
+int pop();
+int peek();
+int isEmpty();
+int isFull();
+void display();
+
+int main()
+{
+    int choice,item;
+    while(1)
+    {
+        printf("1.Push\n");
+        printf("2.Pop\n");
+        printf("3.Display all stack elements\n");
+        printf("4.Quit\n");
+        printf("\nEnter your choice : ");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+        case 1 :
+            printf("\nEnter the item to be pushed : ");
+            scanf("%d",&item);
+            push(item);
+            break;
+        case 2:
+            item = pop();
+            printf("\nPopped item is : %d\n",item );
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(1);
+        default:
+            printf("\nWrong choice\n");
+        }
+    }
+
+    return 0;
+
+}
+
+void push(int item)
+{
+    if( isFull() )
+    {
+        printf("\nStack Overflow\n");
+        return;
+    }
+    top = top+1;
+    stack_arr[top] = item;
+}
+
+int pop()
+{
+    int item;
+    if( isEmpty() )
+    {
+        printf("\nStack Underflow\n");
+        exit(1);
+    }
+    item = stack_arr[top];
+    top = top-1;
+    return item;
+}
+
+int isEmpty()
+{
+    if( top == -1 )
+        return 1;
+    else
+        return 0;
+}
+
+int isFull()
+{
+    if( top == MAX-1 )
+        return 1;
+    else
+        return 0;
+}
+
+void display()
+{
+    int i;
+    if( isEmpty() )
+    {
+        printf("\nStack is empty\n");
+        return;
+    }
+    printf("\nStack elements :\n\n");
+    for(i=top; i>=0; i--)
+        printf(" %d\n", stack_arr[i] );
+    printf("\n");
+}
